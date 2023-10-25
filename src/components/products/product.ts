07d7f56162623c51fa5dec93298f8bdc2b407b50
Alihan Sencan <ge78cug@mytum.de>
2023-10-25 16:35:29 +0200

    Products + ReadMe

---
+++ src/components/products/product.ts
@@ -0,0 +1,9 @@
+export class Product {
+    static id :Number = 0;
+    name: String;
+    code: String;
+    constructor(name : String,code: Number){
+        name = name;
+        code = code;
+    }
+}
