fa2d6b23e19a8cb15745df7c3360bf4ac660d890
ge96qat <ge96qat@mytum.de>
2023-10-27 18:26:15 +0200

    db connection

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,1 +1,17 @@
 //Create a database for the products.
+//
+
+import mysql from 'mysql';
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '<PASSWORD>',
+    database: 'products_db'
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('connected as id'+ connection.threadId);
+});
