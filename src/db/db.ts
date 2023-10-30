44351f12038c66f4477fb0b22bd95f22017c8a34
Alihan Sencan <ge78cug@mytum.de>
2023-10-30 17:13:31 +0100

    Database

--- src/db/db.ts
+++ src/db/db.ts
@@ -9,17 +9,30 @@ import {Tree} from '../components/products/plants/tree'
 import {GoatFertilizer} from '../components/products/fertilizers/goatFertilizer'
 import {SheepFertilizer} from '../components/products/fertilizers/sheepFertilizer'
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '<PASSWORD>',
-    database: 'products_db'
-});
+
+export class Database {
+
+private connection;
+
+constructor(){
+        this.connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: '<PASSWORD>',
+        database: 'products_db'
+    }); 
+    this.connection.connect((err) => {
+        if (err) throw err;
+        console.log('connected as id'+ this.connection.threadId);
+    });
+};
 
 
 
-function addProduct(element) {
+
+
+addProduct(element) {
     
     let sqlString: String[];
     if(element instanceof Plant){
@@ -44,17 +57,14 @@ function addProduct(element) {
             sqlString[2] = 'sheepFertilizer'
     }}
     
-    const query = "INSERT INTO products (name, type, color, weight) VALUES (?, ?)";
-    connection.query(query, [sqlString[0], sqlString[1],sqlString[2],sqlString[3],sqlString[4]], (err, results) => {
+    const db = "INSERT INTO products (name, type, color, weight) VALUES (?, ?, ?, ?, ?)";
+
+    this.connection.query(db, [sqlString[0], sqlString[1],sqlString[2],sqlString[3],sqlString[4]], (err, results) => {
         if (err) throw err;
         console.log(`Added product: ${sqlString[1]} with type: ${sqlString[2]} with color: ${sqlString[3]} with weight: ${sqlString[4]}`);
     });
 }
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log('connected as id'+ connection.threadId);
-});
 
 
-
+}
