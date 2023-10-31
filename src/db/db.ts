18a4faa95c7364e445f31e6d1aa3adc90e4349b1
Alihan Sencan <ge78cug@mytum.de>
2023-10-31 21:02:22 +0100

    DB Optimized

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,81 +1,80 @@
-//Create a database for the products.
-//
-
 import mysql from 'mysql';
-import {Plant} from '../components/products/plants/plant'
-import {Algeae} from '../components/products/plants/algeae'
-import {Flower} from '../components/products/plants/flower'
-import {Tree} from '../components/products/plants/tree'
-import {GoatFertilizer} from '../components/products/fertilizers/goatFertilizer'
-import {SheepFertilizer} from '../components/products/fertilizers/sheepFertilizer'
-
+import { Plant } from '../components/products/plants/plant'
+import { Algeae } from '../components/products/plants/algeae'
+import { Flower } from '../components/products/plants/flower'
+import { Tree } from '../components/products/plants/tree'
+import { GoatFertilizer } from '../components/products/fertilizers/goatFertilizer'
+import { SheepFertilizer } from '../components/products/fertilizers/sheepFertilizer'
 
 export class Database {
+    private connection;
 
-private connection;
-
-constructor(){
+    constructor() {
         this.connection = mysql.createConnection({
-        host: 'localhost',
-        port: 3306,
-        user: 'root',
-        password: '<PASSWORD>',
-        database: 'products_db'
-    }); 
-    this.connection.connect((err) => {
-        if (err) throw err;
-        console.log('connected as id'+ this.connection.threadId);
-    });
-};
+            host: 'localhost',
+            port: 3306,
+            user: 'root',
+            password: '<PASSWORD>',
+            database: 'products_db'
+        });
+        this.connection.connect((err) => {
+            if (err) throw err;
+            console.log('connected as id' + this.connection.threadId);
+        });
+    }
 
-
-addProduct(element) {
-    
-    let sqlString: String[];
-    if (element instanceof Plant) {
-        sqlString = [element.getCode.toString(), 'plant', '', element.getColor.toString(), ''];
-        if (element instanceof Tree) {
-            sqlString[2] = 'tree';
-        } else if (element instanceof Algeae) {
-            sqlString[2] = 'algeae';
-        } else if (element instanceof Flower) {
-            sqlString[2] = 'flower';
+    private constructProductSQL(element): string[] {
+        if (element instanceof Plant) {
+            const type = element instanceof Tree ? 'tree' :
+                element instanceof Algeae ? 'algeae' :
+                element instanceof Flower ? 'flower' : '';
+            return [element.getCode.toString(), 'plant', type, element.getColor.toString(), ''];
+        } else {
+            const type = element instanceof GoatFertilizer ? 'goatFertilizer' :
+                element instanceof SheepFertilizer ? 'sheepFertilizer' : '';
+            return [element.getCode.toString(), 'fertilizer', type, '', element.weight.toString()];
         }
     }
-     else{ 
-         sqlString = [element.getCode.toString(),'fertilizer', '', '',element.weight.toString()]
-        if(element instanceof GoatFertilizer){
-            sqlString[2] =  'goatFertilizer'
 
-    } else if(element instanceof SheepFertilizer){
-            sqlString[2] = 'sheepFertilizer'
-    }}
-    
-    const db = "INSERT INTO products (code, name, type, color, weight) VALUES (?, ?, ?, ?, ?)";
+    addProduct(element) {
+        const sqlString = this.constructProductSQL(element);
+        const db = "INSERT INTO products (code, name, type, color, weight) VALUES (?, ?, ?, ?, ?)";
+        this.connection.query(db, sqlString, (err) => {
+            if (err) throw err;
+            console.log(`Added product: ${sqlString[1]} with type: ${sqlString[2]} with color: ${sqlString[3]} with weight: ${sqlString[4]}`);
+        });
+    }
 
-    this.connection.query(db, [sqlString[0], sqlString[1],sqlString[2],sqlString[3],sqlString[4]], (err, results) => {
-        if (err) throw err;
-        console.log(`Added product: ${sqlString[1]} with type: ${sqlString[2]} with color: ${sqlString[3]} with weight: ${sqlString[4]}`);
-    });
-}
+    private executeQuery(query: string, param: string, successMsg: string) {
+        this.connection.query(query, [param], (err, results) => {
+            if (err) throw err;
+            if (results === null) return 'Found Nothing';
+            console.log(successMsg);
+            for (const res in results) console.log(res);
+        });
+    }
 
-removeProductByCode(productCode) {
-    const db = "DELETE FROM products WHERE code = ?";
-    this.connection.query(db, [productCode], (err, results) => {
-        if (err) throw err;
-        console.log(`Removed product with code: ${productCode}`);
-    });
-}
+    removeProductByCode(productCode) {
+        this.executeQuery("DELETE FROM products WHERE code = ?", productCode, `Removed product with code: ${productCode}`);
+    }
 
-//search product method
-searchProductByCode(productCode) {
-const db = "SELECT * FROM products WHERE code = ?";
-this.connection.query(db, [productCode], (err, results) => {
-    if (err) throw err;
-    console.log(`Found product with code: ${productCode}`);
-    
-});
+    searchProductByCode(productCode) {
+        this.executeQuery("SELECT * FROM products WHERE code = ?", productCode, `Found product with code: ${productCode}`);
+    }
 
-}
+    filterProductByName(productName) {
+        this.executeQuery("SELECT * FROM products WHERE name = ?", productName, `Found product(s) with name: ${productName}`);
+    }
 
+    filterProductByType(productType) {
+        this.executeQuery("SELECT * FROM products WHERE type = ?", productType, `Found product(s) with type: ${productType}`);
+    }
+
+    filterProductByColor(productColor) {
+        this.executeQuery("SELECT * FROM products WHERE color = ?", productColor, `Found product(s) with color: ${productColor}`);
+    }
+
+    filterProductByWeight(productWeight) {
+        this.executeQuery("SELECT * FROM products WHERE weight = ?", productWeight, `Found product(s) with weight: ${productWeight}`);
+    }
 }
